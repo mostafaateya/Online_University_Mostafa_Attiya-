94c635c49db570e0a22b89db0ccdc2a80471f262
mostafaateya <77333503+mostafaateya@users.noreply.github.com>
2023-12-22 01:28:22 +0200

    Update

--- python-flask-E-learning-platform-master/main.py
+++ python-flask-E-learning-platform-master/main.py
@@ -200,7 +200,8 @@ def backup():
         os.stat(TODAYBACKUPPATH)
     except:
         # os.walk(TODAYBACKUPPATH)
-        os.chdir(TODAYBACKUPPATH)
+        # os.chdir(TODAYBACKUPPATH)
+        os.mkdir(TODAYBACKUPPATH)
     db = DB_NAME
     # dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
     dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + command[0] + "/" + db + ".sql"
