0df14e65ba596439cbb55d02b7bfcaf9c2b3d25b
mostafaateya <77333503+mostafaateya@users.noreply.github.com>
2023-12-22 01:59:11 +0200

    Update

--- python-flask-E-learning-platform-master/main.py
+++ python-flask-E-learning-platform-master/main.py
@@ -204,7 +204,7 @@ def backup():
     os.system("mkdir " + TODAYBACKUPPATH)
     db = DB_NAME
     # dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
-    dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + db + " > " + command[0] + "/" + db + ".sql"
+    dumpcmd = "mysqldump -h " + DB_HOST + " -u " + DB_USER + " -y -p" + DB_USER_PASSWORD + " " + db + " > " + command[0] + "/" + db + ".sql"
     os.system(dumpcmd)
     # gzipcmd = "gzip " + pipes.quote(TODAYBACKUPPATH) + "/" + db + ".sql"
     gzipcmd = "gzip " + command[0] + "/" + db + ".sql"
